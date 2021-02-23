73593b1c884d99b99ac40de352e0734a7df7b2ef
FPDPanda <jeanjacques7.jj@gmail.com>
2021-02-23 19:49:23 -0300

    NLW#4 - Trilha ReactJS Day 01

---
+++ nlw04/src/App.tsx
@@ -0,0 +1,13 @@
+// CSS
+import './styles/global.css';
+
+// Components
+import ExperienceBar from './components/ExperienceBar';
+
+export default function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
